7c13c58b7f6bd6b9deb5db66b09bf3a77989c17c
Erik Tjernlund <erik@tjernlund.net>
2022-06-18 19:57:34 +0200

    Add documentation to the restricted log type.

--- Sources/MiamiStateMachine/RestrictedLog.swift
+++ Sources/MiamiStateMachine/RestrictedLog.swift
@@ -1,29 +1,44 @@
 import DequeModule
 
+/// A log for logging generic elements, but having the log
+/// restricted to a limited amount of elements. If the max
+/// capacity has been reached, the oldest element is dropped
+/// from the log.
+///
+/// The log is implemented with the Apple Swift Collection
+/// deque and should be performant.
 public struct RestrictedLog<Element: Hashable> {
     private var log: Deque<Element> = []
     private var capacity: Int
     
+    /// Peek at the latest element in the log.
     public var peek: Element? {
         return log.last
     }
     
+    /// Peek at the oldest element in the log.
     public var peekOldest: Element? {
         return log.first
     }
     
+    /// Is the log empty.
     public var isEmpty: Bool {
         return log.isEmpty
     }
     
+    /// Count of log entries.
     public var count: Int {
         return log.count
     }
     
+    /// Create a log with a max capacity.
+    /// - Parameter capacity: The max capacity.
     public init(capacity: Int) {
         self.capacity = capacity
     }
     
+    /// Push a new element to the log.
+    /// - Parameter element: Element to be pushed on the log.
     public mutating func push(_ element: Element) {
         if count < capacity {
             log.append(element)
@@ -33,7 +48,15 @@ public struct RestrictedLog<Element: Hashable> {
         }
     }
     
+    /// Pop the newest/last element from the log.
+    /// - Returns: Newest element in the log.
     public mutating func pop() -> Element? {
         return log.popLast()
-    }    
+    }
+    
+    /// Pop the oldest/first element from the log.
+    /// - Returns: Oldest element in the log.
+    public mutating func popOldest() -> Element? {
+        return log.popFirst()
+    }
 }
